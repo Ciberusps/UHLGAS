787abaa1418b3696dced2616c7e3e375f5b46f61
Rakudaiii <100479569+Rakudaiii@users.noreply.github.com>
2025-12-17 14:20:44 +0500

    Merge pull request #1 from Rakudaiii/main
    add AT_OnTick

---
+++ Source/UHLGAS/Private/Tasks/AT_OnTick.cpp
@@ -0,0 +1,29 @@
+﻿// Copyright (c) 2024 NextGenium
+
+
+#include "Tasks/AT_OnTick.h"
+
+UAT_OnTick::UAT_OnTick(const FObjectInitializer& ObjectInitializer)
+{
+	bTickingTask = true;
+}
+ 
+UAT_OnTick* UAT_OnTick::AbilityTaskOnTick(UGameplayAbility* OwningAbility, FName TaskInstanceName)
+{
+	UAT_OnTick* MyObj = NewAbilityTask<UAT_OnTick>(OwningAbility, TaskInstanceName);
+	return MyObj;
+}
+ 
+void UAT_OnTick::Activate()
+{
+	Super::Activate();
+}
+ 
+void UAT_OnTick::TickTask(float DeltaTime)
+{
+	Super::TickTask(DeltaTime);
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		OnTick.Broadcast(DeltaTime);
+	}
+}
---
+++ Source/UHLGAS/Public/Tasks/AT_OnTick.h
@@ -0,0 +1,33 @@
+﻿// Copyright (c) 2024 NextGenium
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "AT_OnTick.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnTickTaskDelegate, float, DeltaTime);
+
+/**
+ * 
+ */
+UCLASS()
+class UHLGAS_API UAT_OnTick : public UAbilityTask
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintAssignable)
+	FOnTickTaskDelegate OnTick;
+ 
+public:
+ 
+	UAT_OnTick(const FObjectInitializer& ObjectInitializer);
+ 
+	UFUNCTION(BlueprintCallable, Category = "Ability|Tasks", meta = (HidePin = "OwningAbility", DefaultToSelf = "OwningAbility", BlueprintInternalUseOnly = "TRUE"))
+	static UAT_OnTick* AbilityTaskOnTick(
+		UGameplayAbility* OwningAbility,
+		FName TaskInstanceName);
+	
+	virtual void Activate() override;
+	virtual void TickTask(float DeltaTime) override;
+};
